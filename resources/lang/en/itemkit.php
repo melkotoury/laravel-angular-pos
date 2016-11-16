755a230008bc80438fbd223d6f097913c8d61fc1
melkotoury <melkotoury@gmail.com>
2016-11-16 22:23:18 +0200

    refactor: update english translation

--- resources/lang/en/itemkit.php
+++ resources/lang/en/itemkit.php
@@ -2,20 +2,20 @@
 
 return [
 
-	'item_kits'	=> 'Category',
-	'new_item_kit'		=> 'New Category',
-	'item_kit_id' 		=> 'Category ID',
-	'item_kit_name' 	=> 'Category Name',
+	'item_kits'	=> 'Products',
+	'new_item_kit'		=> 'New Product',
+	'item_kit_id' 		=> 'Product ID',
+	'item_kit_name' 	=> 'Product Name',
 	'cost_price' 	=> 'Cost Price',
 	'selling_price' => 'Selling Price',
-	'item_kit_description'		=> 'Category Description',
+	'item_kit_description'		=> 'Product Description',
 	'search_item' => 'Search Item:',
 	'description' => 'Description',
 	'quantity' => 'Quantity',
 	'profit' => 'PROFIT:',
 	'item_id' => 'Item ID',
 	'item_name' => 'Item Name',
-	'submit' => 'Submit Category',
+	'submit' => 'Submit Product',
 
 
 ];
